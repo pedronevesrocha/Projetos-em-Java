b9906a2554cda213ebf25a63ea7b632459826ba2
Pedro Rocha <pedrofrancisconeves@yahoo.com.br>
2022-10-04 11:24:43 -0300

    add more exercises

---
+++ 20 exercicios em Java/src/media3.java
@@ -0,0 +1,9 @@
+
+public class media3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 20 exercicios em Java/src/notasMoedas.java
@@ -0,0 +1,9 @@
+
+public class notasMoedas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
